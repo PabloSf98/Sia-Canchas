3415102396272e4d0a9656e8ff3a7ff653a96e95
Pabloskirt <50030789+Pabloskirt@users.noreply.github.com>
2019-05-21 16:21:35 -0400

    Conexion con la BDD
    Base de datos con su respectiva conexion a java

---
+++ Canchas/src/Conexiones/Conexion.java
@@ -0,0 +1,37 @@
+package Conexiones;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+import java.sql.Statement;
+public class Conexion {
+    static Connection contacto=null;
+    public static String usuario;
+    public static String password;
+    public static boolean status=false;
+    public static Connection getConexion(){
+        String url="jdbc:sqlserver://localhost:1433;databaseName=MonumentalSoccer";
+        try{
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        }catch(ClassNotFoundException e){
+                JOptionPane.showMessageDialog(null,"No se pudo establecer la conexion... revisar Driver" + e.getMessage(),
+                "Error de conexion",JOptionPane.ERROR_MESSAGE);
+        }
+        try{
+            contacto = DriverManager.getConnection(url,usuario,password);
+            status=true;
+        }catch (SQLException e){
+            JOptionPane.showMessageDialog(null,"Error" + e.getMessage(),
+            "Error de conexion",JOptionPane.ERROR_MESSAGE);
+        }
+        return contacto;
+    }
+    public static void setCuenta(String usuario, String password){
+        Conexion.usuario=usuario;
+        Conexion.password=password;
+    }
+    public static boolean getstatus(){
+        return status;
+    }
+}
